df49fd99486bd5b35dfea5839626dc38f7f90ca8
mileslucey <43838442+mileslucey@users.noreply.github.com>
2018-12-08 10:08:30 -0800

    UCBBEL Homework #3

---
+++ PyBank/main.py
@@ -0,0 +1,61 @@
+# imports
+import os
+import csv
+# establishing paths
+csv_path = os.path.join('03-Python_Homework 3 Python_Instructions_PyBank_Resources_budget_data.csv')
+output_path = os.path.join("output.txt")
+# establishing variables to help with organization
+total_months = 0
+net_pl = 0
+total_months_two = 0
+first_iteration = True
+previous_pl = 0
+current_pl = 0
+difference_pl = 0
+difference_pl_list = []
+avg_change = 0
+greatest_increase_date = 0
+greatest_increase_amount = 0
+greatest_decrease_date = 0
+greatest_decrease_amount = 0
+# csv reader
+with open(csv_path,newline = '') as csv_file:
+	csv_reader = csv.reader(csv_file,delimiter = ',')
+	next(csv_reader,None)
+# loops that find the total months, total profit/loss, greatest increase in profit/loss, and greatest decrease in profit/loss
+	for row in csv_reader:
+		total_months += 1
+		net_pl += float(row[1])
+# if statement created to skip one iteration because we cannot calculate the difference in profit/loss on the first iteration (nothing to subtract)
+		if first_iteration:
+			first_iteration = False
+		else:
+			current_pl = float(row[1])
+			difference_pl = current_pl - previous_pl
+			difference_pl_list.append(difference_pl)
+# if statement to find the greatest increase and decrease in profit/loss
+			if difference_pl > greatest_increase_amount:
+				greatest_increase_amount = difference_pl
+				greatest_increase_date = row[0]
+			elif difference_pl < greatest_decrease_amount:
+				greatest_decrease_amount = difference_pl
+				greatest_decrease_date = row[0]
+			else:
+				pass 
+		previous_pl = float(row[1])
+# calculates average change
+avg_change = sum(difference_pl_list) / (total_months-1)
+# output
+output_information = (
+	f"\nFinancial Analysis\n"
+	f"\n----------------------------\n"
+	f"\nTotal Months: {total_months}\n"
+	f"\nTotal: ${net_pl}\n"
+	f"\nAverage Change: ${avg_change}\n"
+	f"\nGreatest Increase in Profits: ${greatest_increase_amount} in {greatest_increase_date}\n"
+	f"\nGreatest Decrease in Profits: ${greatest_decrease_amount} in {greatest_decrease_date}\n"
+	)
+print(output_information)
+# exports outlet to text file
+with open(output_path,"w",newline = '') as txt_output_file:
+	txt_output_file.write(output_information)	
---
+++ PyPoll/main.py
@@ -0,0 +1,73 @@
+# imports
+import os
+import csv
+# establishing paths
+csv_path = os.path.join('03-Python_Homework 3 Python_Instructions_PyPoll_Resources_election_data.csv')
+output_path = os.path.join("output.txt")
+# establishing variables to help with organization
+total_votes = 0
+khan_votes = 0
+khan_percentage = 0
+correy_votes = 0
+correy_percentage = 0
+li_votes = 0
+li_percentage = 0
+otooley_votes = 0
+otooley_percentage = 0
+winning_votes = 0
+winner = 0
+# csv reader
+with open(csv_path,newline = '') as csv_file:
+	csv_reader = csv.reader(csv_file,delimiter = ',')
+	next(csv_reader,None)
+# loop to count total votes and # of votes for each candidate
+	for row in csv_reader:
+		total_votes += 1
+		if row[2] == "Khan":
+			khan_votes +=1
+		elif row[2] == "Correy":
+			correy_votes +=1
+		elif row[2] == "Li":
+			li_votes +=1
+		elif row[2] == "O'Tooley":
+			otooley_votes +=1
+		else:
+			pass
+# calculate percentages
+	khan_percentage = round((khan_votes/total_votes) * 100,3)
+	correy_percentage = round((correy_votes/total_votes) * 100,3)
+	li_percentage = round((li_votes/total_votes) * 100,3)
+	otooley_percentage = round((otooley_votes/total_votes) * 100,3)
+# loop to find the winner
+	if khan_votes > winning_votes:
+		winning_votes = khan_votes
+		winner = "Khan"
+	elif correy_votes > winning_votes:
+		winning_votes = correy_votes
+		winner = "Correy"
+	elif li_votes > winning_votes:
+		winning_votes = li_votes
+		winner = "Li"
+	elif otooley_votes > winning_votes:
+		winning_votes = otooley_votes
+		winner = "O'Tooley"
+	else:
+		pass
+# output
+output_information = (
+	f"\nElection Results\n"
+	f"\n----------------------------\n"
+	f"\nTotal Votes: {total_votes}\n"
+	f"\n----------------------------\n"
+	f"\nKhan: {khan_percentage}% ({khan_votes})\n"
+	f"\nCorrey: {correy_percentage}% ({correy_votes})\n"
+	f"\nLi: {li_percentage}% ({li_votes})\n"
+	f"\nO'Tooley: {otooley_percentage}% ({otooley_votes})\n"
+	f"\n----------------------------\n"
+	f"\nWinner: {winner}\n"
+	f"\n----------------------------\n"
+	)
+print(output_information)
+# exports outlet to text file
+with open(output_path,"w",newline = '') as txt_output_file:
+	txt_output_file.write(output_information)	
